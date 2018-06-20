e84f153fec20028fbc0b8f235bab4d2fc1c4cf00
陈景远 <rocky.cn@foxmail.com>
2018-06-20 17:40:55 +0800

    Revert "Merge pull request #97 from torsten-sauer/master"
    This reverts commit 9eb2208ed9748c2498b4a339d1b00485b7efbafa, reversing
    changes made to 3b8aaf205cb6f7c0bb8a153764c11ad0a3a47e09.

--- src/tree-grid-directive.js
+++ src/tree-grid-directive.js
@@ -135,7 +135,7 @@
               if (scope.treeData.length) {
                 var _col_defs = [],
                   _firstRow = scope.treeData[0],
-                  _unwantedColumn = ['children', 'level', 'expanded', 'icons', expandingProperty];
+                  _unwantedColumn = ['children', 'level', 'expanded', expandingProperty];
                 for (var idx in _firstRow) {
                   if (_unwantedColumn.indexOf(idx) === -1) {
                     _col_defs.push({
@@ -364,12 +364,12 @@
                   branch.expanded = false;
                 }
                 if (!branch.children || branch.children.length === 0) {
-                  tree_icon = branch.icons && branch.icons.iconLeaf || attrs.iconLeaf;
+                  tree_icon = attrs.iconLeaf;
                 } else {
                   if (branch.expanded) {
-                    tree_icon = branch.icons && branch.icons.iconCollapse || attrs.iconCollapse;
+                    tree_icon = attrs.iconCollapse;
                   } else {
-                    tree_icon = branch.icons && branch.icons.iconExpand || attrs.iconExpand;
+                    tree_icon = attrs.iconExpand;
                   }
                 }
                 branch.level = level;
